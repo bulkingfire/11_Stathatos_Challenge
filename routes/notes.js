803ff575a54dc179d69ebe3adef0f6a89b18e345
bulkingfire <118399104+bulkingfire@users.noreply.github.com>
2023-03-05 11:50:05 -0600

    Add files via upload

---
+++ routes/notes.js
@@ -0,0 +1,31 @@
+const texts = require('express').Router();
+const para1 = require('../extra/help1');
+const  { readFromFile, writeToFile, readAndAppend } = require('../extra/help2');
+const db = require('../db/db.json');
+
+
+texts.get('/notes', (req,res) => {
+
+    console.log(`${req.method} request has been cleared`);
+  readFromFile('db/db.json', 'utf8').then((data) => res.json(JSON.parse(data)));
+})
+
+texts.post('/notes', (req,res) => {
+  console.log(`${req.method} request has been cleared`);
+  
+readFromFile('db/db.json', 'utf8').then((data) => {
+  var notes = new Array();
+  notes.push({
+    ...req.body, 
+    id:para1()
+  })
+
+  writeToFile('db/db.json', JSON.stringify(notes))
+  
+  res.json(notes[notes.length-1])
+
+}
+);
+})
+
+module.exports = texts;
